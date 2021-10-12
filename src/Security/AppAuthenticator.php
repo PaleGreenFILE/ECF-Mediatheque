7a85fa915915cc7abd0231eb1eeb0b63026d7ced
Papoel <pascal.briffard@gmail.com>
2021-10-12 14:47:23 +0200

    composer update + fix redirection de l'utilisateur lors de sa connexion

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -46,14 +46,14 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
-        
+        // if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        //     return new RedirectResponse($targetPath);
+        // }
+
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('app_home'));
         return new RedirectResponse($this->urlGenerator->generate('access'));
-        
+
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 

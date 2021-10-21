3f65f2b228a90a3fac5d3c2b197d05b60075cf46
Papoel <pascal.briffard@gmail.com>
2021-10-21 15:22:50 +0200

    migration prete pour Heroku

--- migrations/Version20211021131800.php
+++ migrations/Version20211021131800.php
@@ -10,27 +10,35 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211019170538 extends AbstractMigration
+final class Version20211021131800 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Création du schéma complet de base de donné et insertion de donnée de base';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE genre (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('CREATE TABLE livre (id INT AUTO_INCREMENT NOT NULL, genre_id INT DEFAULT NULL, titre VARCHAR(100) NOT NULL, file VARCHAR(255) DEFAULT NULL, parution DATE DEFAULT NULL, description LONGTEXT DEFAULT NULL, auteur VARCHAR(255) NOT NULL, quantite INT NOT NULL, isbn VARCHAR(13) DEFAULT NULL, pret INT DEFAULT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_AC634F994296D31F (genre_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, is_validate TINYINT(1) NOT NULL, is_restitue TINYINT(1) NOT NULL, emprunted_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', status VARCHAR(255) NOT NULL, INDEX IDX_42C84955A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('CREATE TABLE reservation_livre (reservation_id INT NOT NULL, livre_id INT NOT NULL, INDEX IDX_EF1C9F3EB83297E7 (reservation_id), INDEX IDX_EF1C9F3E37D925CB (livre_id), PRIMARY KEY(reservation_id, livre_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, date_naissance DATE NOT NULL, adresse LONGTEXT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', is_verified TINYINT(1) NOT NULL, is_autorise TINYINT(1) NOT NULL, auth_code VARCHAR(255) DEFAULT NULL, emprunt_max INT DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
         $this->addSql('ALTER TABLE livre ADD CONSTRAINT FK_AC634F994296D31F FOREIGN KEY (genre_id) REFERENCES genre (id)');
+
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+
         $this->addSql('ALTER TABLE reservation_livre ADD CONSTRAINT FK_EF1C9F3EB83297E7 FOREIGN KEY (reservation_id) REFERENCES reservation (id) ON DELETE CASCADE');
+
         $this->addSql('ALTER TABLE reservation_livre ADD CONSTRAINT FK_EF1C9F3E37D925CB FOREIGN KEY (livre_id) REFERENCES livre (id) ON DELETE CASCADE');
 
-        // Inserer un admin
+                // Inserer un admin
         $this->addSql("INSERT INTO users (nom, prenom, date_naissance, adresse, email, password, roles, is_verified, is_autorise, auth_code, emprunt_max) values('Briffard','Pascal','1985-02-20','7 rue du col blanc 59245 Recquignies','admin@email.fr','\$2y\$13\$TDoGH80tbKITua5pPWqSou.TyMSadWszqsQSnxO9W0/VSq5rC0cQK','[\"ROLE_ADMIN\"]','1', '1', '123456','999')");
 
         // Inserer un Employé
@@ -77,13 +85,21 @@ final class Version20211019170538 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE livre DROP FOREIGN KEY FK_AC634F994296D31F');
+
         $this->addSql('ALTER TABLE reservation_livre DROP FOREIGN KEY FK_EF1C9F3E37D925CB');
+
         $this->addSql('ALTER TABLE reservation_livre DROP FOREIGN KEY FK_EF1C9F3EB83297E7');
+
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
+
         $this->addSql('DROP TABLE genre');
+
         $this->addSql('DROP TABLE livre');
+
         $this->addSql('DROP TABLE reservation');
+
         $this->addSql('DROP TABLE reservation_livre');
+
         $this->addSql('DROP TABLE users');
     }
 
